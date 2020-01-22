e93da64d8c512c5ce242f8fb0750dd57936d2805
Brandon Horlacher <brandon.horlacher@telenotes.com>
2020-01-21 22:49:44 -0700

    finishing basic experiment for spring boot websockets.

---
+++ src/main/java/websockets/TextSocketHandler.java
@@ -0,0 +1,18 @@
+package websockets;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class TextSocketHandler extends TextWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        JSONObject jsonObject = new JSONObject(payload);
+
+        session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + ", how may we help you?"));
+    }
+}
---
+++ src/main/java/websockets/driver.java
@@ -0,0 +1,11 @@
+package websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class driver {
+    public static void main(String[] args) {
+        SpringApplication.run(driver.class, args);
+    }
+}
